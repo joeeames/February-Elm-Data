0d10d5c30512305a226cff7485649cea30ccd21c
Joe Eames <joeeames@gmail.com>
2016-02-10 23:08:12 -0700

    added first version of files

---
+++ Characters.elm
@@ -0,0 +1,16 @@
+module Characters where
+
+characterList = List String
+characterList = 
+	[ "Poe Dameron",
+		"BB8",
+		"Captain Phasma",
+		"Rey",
+		"Finn",
+		"Kylo Ren",
+		"Luke Skywalker",
+		"Chewbacca",
+		"Han Solo",
+		"Supreme Leader Snoke",
+		"Princess Leia"
+	]
