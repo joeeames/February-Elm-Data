4c4dc000ece8a0b4003d0c5a43342b3998adc017
Murphy Randle <murphyrandle@gmail.com>
2016-02-11 19:43:11 -0700

    added celebs

---
+++ App/Celebrities.elm
@@ -0,0 +1,17 @@
+module Celebrities (..) where
+
+
+celebrityList : List String
+celebrityList =
+    [ "Cristiano Ronaldo"
+    , "Kim Kardashian"
+    , "Kanye West"
+    , "Amy Schumer"
+    , "Kevin Hart"
+    , "Zach Galifianakis"
+    , "Zac Efron"
+    , "Donald Trump"
+    , "Hilary Clinton"
+    , "Nicolas Cage"
+    , "Miley Cyrus"
+    ]
